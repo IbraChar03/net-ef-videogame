0d86b2a222a38c31a5ac905d0ed7479745812926
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-21 14:33:41 +0200

    Add project files.

---
+++ net-ef-videogame/SoftwareHouse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    internal class SoftwareHouse
+    {
+        public int SoftwareHouseID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Videogame> Videogames { get; set;}
+    }
+}
---
+++ net-ef-videogame/Videogame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    internal class Videogame
+    {
+        public int VideogameID { get; set; }
+        public string Name { get; set; }
+        public string Overview { get; set; }
+        public DateTime release_date {  get; set; }
+        public int SoftwareHouseID { get; set; }
+    }
+}
